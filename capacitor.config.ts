83857c1ea7e8d63465a4f0bdb5b6dbb0f0e57d24
Deijai Miranda Almeida <deijai.almeida@totvs.com.br>
2023-03-04 10:45:34 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Meu Coletor de Dados TOTVS',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
